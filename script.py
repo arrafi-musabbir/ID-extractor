76cfe68f0c75baa626c7d84f76dea50eb02d9314
arrafi-musabbir <musabbir.arrafi@gmail.com>
2022-07-30 23:18:46 +0600

    reading ids and sensors

--- script.py
+++ script.py
@@ -4,30 +4,44 @@ import re
 
 class extractor:
     
-    def __init__(self, data):
-        # self.data = data
-        self.fmt = data[:2]
-        self.read_json()
+    def __init__(self, data, dformat):
+        self.read_json(dformat)
         self.extract_values(data)
 
-    def read_json(self):
+    def read_json(self, dformat):
         with open(os.path.join(os.getcwd(),'data.json'),'r') as file:
             data = json.load(file)
-            self.extract_fields(data)
+            self.extract_fields(data, dformat)
     
-    def extract_fields(self, fmt_data):
-        self.seps = fmt_data[self.fmt]['sep']
-        self.ids = fmt_data[self.fmt]['ids']
-        self.sensors = fmt_data[self.fmt]['sensors']
+    def extract_fields(self, fmt_data, dformat):
+        self.seps = fmt_data[dformat]['sep']
+        self.ids = fmt_data[dformat]['ids']
+        self.sensors = fmt_data[dformat]['sensors']
 
     def extract_values(self, data):
-        temp = data.split(self.seps[0])
         regexPattern = '|'.join(map(re.escape, tuple(self.seps)))
-        print(re.split(regexPattern, data))  
+        data = re.split(regexPattern, data)
+        return {**self.extract_ids(data) , **self.extract_sensors(data)} 
+    
+    def extract_ids(self, data):
+        i = 0
+        for key in self.ids.keys():
+            self.ids[key] = data[i]
+            i+=1 
+        return self.ids 
+            
+    def extract_sensors(self, data):
+        i = len(self.ids)
+        for key in self.sensors.keys():
+            self.sensors[key] = data[i]
+            i+=1 
+        return self.sensors  
 
 
         
 
-data = 'f1abcd,erefe@234424 45235!935235'
+data = 'abcd,erefe@234424 45235!935235'
+dformat = 'f1'
 
-o1 = extractor(data)
+o1 = extractor(data, dformat)
+print(o1)
