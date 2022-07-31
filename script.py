c6d7cc0a95fab2e4bdb8ee643185038d5ed0da53
aurxine <sarowardhrubo@gmail.com>
2022-07-31 16:17:18 +0600

    other formats added

--- script.py
+++ script.py
@@ -46,21 +46,23 @@ class extractor:
 if __name__=="__main__":
     ### example data
     data1 = 'GID:8020002203220000, DID:802102206290004, Analog: 1000 142 0 476'
-    # data2 = 'abcd,erefe,sigrhjs 234424 45235 935235'
-    # data3 = 'abcd,erefe,234424,45235'
+    data2 = ' GID:8020002203220000, connected'
+    data3 = 'GID:8020002203220000, connected'
 
     ### data format
     dfmt1 = 'Dfmt8020001'
-    # dfmt2 = 'f2'
-    # dfmt3 = 'f3'
+    dfmt2 = 'Dfmt8020002'
+    dfmt3 = 'Dfmt8020003'
 
     ### creating object with various data format
     o1 = extractor(dfmt1)
-    # o2 = extractor(dfmt2)
-    # o3 = extractor(dfmt3)
+    o2 = extractor(dfmt2)
+    o3 = extractor(dfmt3)
 
     ### will return dictionary as defined in file.json
     dict1 = o1.__extract__(data1)
+    dict2 = o2.__extract__(data2)
+    dict3 = o3.__extract__(data3)
     
     print(dict1["gateway"])
     print(dict1["device"])
@@ -69,5 +71,10 @@ if __name__=="__main__":
     print(dict1["bat"])
     print(dict1["temperature"])
     
-    # print(o2.__extract__(data2))
-    # print(o3.__extract__(data3))
+    
+    print(dict2["gateway"])
+    print(dict2["status"])
+    
+    
+    print(dict3["gateway"])
+    print(dict3["status"])
